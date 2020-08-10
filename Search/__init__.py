d9fc0db1da0022ed95fa6cb25caca79a109a633a
iamsikun <xusikun96@gmail.com>
2020-08-10 11:41:17 -0400

    Initial commit for Sorting module

---
+++ Search/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 8/8/20 19:10
+# @Author  : Sikun Xu
+# @FileName: __init__.py.py
+# @Software: PyCharm
--- Sort/sort.py
+++ Sort/sort.py
@@ -3,3 +3,44 @@
 # @Author  : Sikun Xu
 # @FileName: sort.py
 # @Software: PyCharm
+
+class Sort:
+    def __init__(self):
+        """
+        TODO
+        """
+        pass
+
+
+class QuickSort(Sort):
+    def __init__(self, arr: list):
+        """
+        TODO
+        """
+        # init
+        super(QuickSort, self).__init__()
+
+        # parameters
+        self.arr = arr
+
+        # attributes
+        pass
+
+        # execute the algorithm
+        self.quick_sort()
+
+    def quick_sort(self):
+        """
+        Reference for quick sort:
+        1. https://en.wikipedia.org/wiki/Quicksort
+        """
+
+        # 1. pick the pivot
+        pivot = self.arr[-1]
+
+        # 2. partitioning
+        for idx in range(-2, -len(self.arr)-1, -1):
+            
+        # 3. recursion
+        pass
+
